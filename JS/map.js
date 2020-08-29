1eba27924d585942398dc95d128ba27821ca5fd8
ehue <steinbeck.ehue@gmail.com>
2020-08-29 02:48:06 +0200

    correction issue responsive et btn play

--- JS/map.js
+++ JS/map.js
@@ -1,6 +1,18 @@
+class Mapview{
+
+    constructor(map,lat,lng,zoom){
+            this.map = map;
+            this.latitude = lat;
+            this.longitude = lng;
+            this.zoom = zoom;
+
+
+    }
+}
+
 let nantes =[47.2186371,-1.5541362];
 //création de la map    
-let map = L.map("map").setView(nantes,10);
+this.map = L.map("map").setView(nantes,10);
 //création du calque image
 L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png',{
     maxZoom:22
@@ -10,3 +22,8 @@ L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png',{
 let marker = L.marker (nantes).addTo(map);
 // ajout de Poppup
 marker.bindPopup("<b>Bienvenue à Nantes !</b>");
+
+let url ="https://api.jcdecaux.com/vls/v1/stations?contract=nantes&apiKey=6d02f7b159f2738f2e25fdc25a261b8d849ec07e"
+
+fetch(url).then (response=> response.json())
+        .then(data=>console.log(data));
